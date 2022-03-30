373e0b4e51650116f4e06228ef7befc6d5aac381
Alexander <valverin95@gmail.com>
2022-03-30 19:44:20 +0300

    feat: updated routers

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,9 @@ export default defineConfig({
     resolve: {
         alias: {
             '@root': __dirname,
-            '@components': path.resolve(__dirname, 'src', 'lib', 'components')
+            '@components': path.resolve(__dirname, 'src', 'lib', 'components'),
+            '@shared': path.resolve(__dirname, 'src', 'shared'),
+            '@views': path.resolve(__dirname, 'src', 'views'),
         }
     },
 });
