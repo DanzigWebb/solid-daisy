90d194d0c0fc9b76c2fed55be50b3b6d461bf242
Alexander <valverin95@gmail.com>
2022-04-14 21:24:13 +0300

    feat: added input component

--- src/lib/components/form/input/Input.tsx
+++ src/lib/components/form/input/Input.tsx
@@ -1,7 +1,53 @@
 import { Component } from 'solid-js';
+import { DaisyColor, DaisySize } from '../../../types';
+import { DOMElement } from 'solid-js/jsx-runtime';
 
-export const Input: Component = (props) => {
-    return (
-        <input type="text"/>
-    )
+type InputEvent = Event & { currentTarget: HTMLInputElement; target: DOMElement }
+
+type InputColor = DaisyColor | 'ghost';
+
+type Props = {
+    placeholder?: string;
+    type?: string;
+    color?: InputColor;
+    size?: DaisySize;
+    bordered?: boolean;
+
+    onChange?: (e: InputEvent) => void;
+    onInput?: (e: InputEvent) => void;
+    onFocus?: (e: InputEvent) => void;
+    onBlur?: (e: InputEvent) => void;
 }
+
+export const Input: Component<Props> = (props) => {
+
+    return (
+        <input
+            type="text"
+            placeholder={props.placeholder}
+            class="input"
+            classList={{
+                'input-lg': props.size === 'lg',
+                'input-md': props.size === 'md',
+                'input-sm': props.size === 'sm',
+                'input-xs': props.size === 'xs',
+
+                'input-primary':   props.color === 'primary',
+                'input-secondary': props.color === 'secondary',
+                'input-accent':    props.color === 'accent',
+                'input-info':      props.color === 'info',
+                'input-success':   props.color === 'success',
+                'input-warning':   props.color === 'warning',
+                'input-error':     props.color === 'error',
+                'input-ghost':     props.color === 'ghost',
+
+                'input-bordered': props.bordered
+            }}
+
+            onChange={props.onChange}
+            onInput={props.onInput}
+            onBlur={props.onBlur}
+            onFocus={props.onFocus}
+        />
+    );
+};
